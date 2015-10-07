b51a2a226b44a3fff387f35f3eff49c3753ee2a6
Morgan Evans <evansmorg@gmail.com>
2015-10-06 18:32:23 -0700

    initial commit

---
+++ Hello/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by iGuest on 10/6/15.
+//  Copyright (c) 2015 iGuest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mainText: UILabel!
+
+
+}
+
